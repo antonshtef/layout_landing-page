05122e4e8b5c6b722d17e9c743733715a27830fe
anton <1288stef1288@gmail.com>
2025-03-27 14:33:27 +0200

    make build

---
+++ index.91ce0aad.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("html");function c(){e.classList.toggle("unscroll")}document.querySelector(".icon--burger").addEventListener("click",c),document.querySelector(".icon--close").addEventListener("click",c),document.querySelector(".nav__list").addEventListener("click",c);
+//# sourceMappingURL=index.91ce0aad.js.map
